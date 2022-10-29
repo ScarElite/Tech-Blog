d8b2bcdd66a678bc941c9dcdf8881bfec850d325
Mitchell Armstrong <mra0211@gmail.com>
2022-10-29 10:45:00 -0400

    Added api routes files to the controllers folder. Added a login funnction in the public/javascript folder. Created the main.handlebars template

---
+++ public/javascript/login.js
@@ -0,0 +1,19 @@
+const signUpBtn = document.querySelector("#signup-button");
+function signUpFormHandler(event) {
+  event.preventDefault();
+  const username = document.querySelector("#username-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+  if (username && email && password) {
+    fetch("/api/users", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    }).then((response) => {
+      console.log(response);
+    });
+  }
+}
